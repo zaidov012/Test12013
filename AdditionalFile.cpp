a5751d36b623c36b663aee8abc15c7180639cf36
Asif Zaidov <zaidov012@gmail.com>
2021-01-06 21:33:37 +0400

    Edit V. 2.0
    Added some test lines

--- AdditionalFile.cpp
+++ AdditionalFile.cpp
@@ -4,5 +4,8 @@ using namespace std;
 int main()
 {
   cout << "Hi, World! This is GitHub's editor and I'm writing code here!" << endl;
+  cout << "Test edit line 1" << endl;
+  cout << "Test edit line 2" << endl;
+  cout << "Test edit line 3" << endl;
   return 0;
 }
